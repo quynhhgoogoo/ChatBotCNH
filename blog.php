ae32820dfe6cdf589790b59662ae0185916d2e58
Diem Quynh LUong <luongdiemquynh1998@gmail.com>
2018-10-19 00:10:08 +0300

    form

---
+++ blog.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<head>
+<title>Chat Form</title>
+</head>
+
+<body>
+Your gender is : <?php echo $_POST["gender"]; ?><br>
+Your CNHer status is : <?php echo $_POST["cnher"]; ?><br>
+
+</body>
+</html>
